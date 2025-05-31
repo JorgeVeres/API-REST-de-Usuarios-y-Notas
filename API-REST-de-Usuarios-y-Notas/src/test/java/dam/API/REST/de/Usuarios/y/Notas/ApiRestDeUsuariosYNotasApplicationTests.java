5aef351750106a3ed153184911b19adfba20e891
JorgeVeres <moganero292@gmail.com>
2025-05-31 14:57:34 +0100

    Initialize Spring Boot project with Maven wrapper and basic configuration files

---
+++ API-REST-de-Usuarios-y-Notas/src/test/java/dam/API/REST/de/Usuarios/y/Notas/ApiRestDeUsuariosYNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package dam.API.REST.de.Usuarios.y.Notas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestDeUsuariosYNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
